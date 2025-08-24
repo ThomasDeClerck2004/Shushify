5b70db7701a9f67bce8d977cecaee3434112218e
Thomas De Clerck <thomas.de.clerck4@gmail.com>
2025-08-24 14:27:00 +0200

    updated bat file and shortcut so it works on all devices (windows) when downloaded

--- Spotify.bat
+++ Spotify.bat
@@ -1,3 +1,3 @@
 @echo off
-start "" "C:\Users\thoma\AppData\Roaming\Spotify\Spotify.exe"
-start "" "C:\Users\thoma\Projects\Personal\Shushify\.venv\Scripts\pythonw.exe" "C:\Users\thoma\Projects\Personal\Shushify\main.py"
+start "" "%APPDATA%\Spotify\Spotify.exe"
+start "" "%~dp0.venv\Scripts\pythonw.exe" "%~dp0main.py"
